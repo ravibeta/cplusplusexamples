ece5203f372653a727105902c0e3eb98bd01f3e3
Ravi Rajamani <ravibeta@hotmail.com>
2014-02-13 21:22:12 -0800

    Create WinMSMQ.h
    untested

---
+++ WinMSMQ.h
@@ -0,0 +1,78 @@
+#ifndef _WIN_MSMQ_MONITOR_H_
+#define _WIN_MSMQ_MONITOR_H_ 1
+#define SECURITY_WIN32 1
+#include <windows.h>
+#include <stdio.h>
+#include <Mq.h>
+
+
+class WinMSMQMonitorThread;
+
+/*
+ *  MSMQ monitor
+ */
+class WinMSMQMonitor
+{
+public:
+    WinMSMQMonitor();
+    virtual ~WinMSMQMonitor(void);
+
+    bool init(void);
+    bool run(void);
+    void stop(void);
+
+
+private:
+    bool StartMonitorWorker(void);
+    void MonitorWorker(void);
+    void processData(UCHAR * pbE, ULONG totalSize);
+
+private:
+
+    WinMSMQMonitorThread *                  	_thread;
+    HANDLE                                      _completionPort;
+    volatile LONG                               _isShuttingDown;
+    HANDLE                                      _shutDownEvt;
+	
+    friend class WinMSMQMonitorThread;
+
+};
+
+class WinMSMQMonitorThread 
+{
+public:
+    WinMSMQMonitorThread () : _WinMSMQMonitor (NULL) {}
+
+    void start(WinMSMQMonitor *WinMSMQMonitor);
+
+private:
+    virtual void main();
+
+    WinMSMQMonitor *_WinMSMQMonitor;
+};
+
+
+class CScopedMSMQWrapper
+{
+	QUEUEHANDLE hQ;
+
+public:
+	CScopedMSMQWrapper()
+	{
+		hQ = INVALID_HANDLE_VALUE;
+	}
+
+	~CScopedMSMQWrapper()
+	{
+            CloseQueue();
+	}
+
+	HRESULT InitQueue(const WCHAR* wczQueueName, size_t qLen, const WCHAR* wczMachineName, size_t mLen, DWORD dwAccessMode, DWORD dwSharedMode);
+        void CloseQueue();
+	HRESULT ReadQueue();
+	HRESULT SendQueue(const WCHAR* wczPayload, size_t nLen);
+	MQMSGPROPS* GetMsgProps();
+	void FreeMsgProps(MQMSGPROPS* pMsgProps, int numProperties);
+};
+
+#endif
